a1847d29e73effddc699c802af510ccd0f0b295a
Zakaria Ismail <1112zakaria@gmail.com>
2023-11-24 00:37:37 -0500

    initial project files

---
+++ src/main/java/leo/satellite/capstoneuiserver/CapstoneUiServerApplication.java
@@ -0,0 +1,13 @@
+package leo.satellite.capstoneuiserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneUiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneUiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leo/satellite/capstoneuiserver/CapstoneUiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package leo.satellite.capstoneuiserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneUiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
